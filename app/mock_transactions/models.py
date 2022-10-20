836c67040d789b99ba40ea31b20212fd31a0d192
Ubuntu <emile@emile-virtual-machine.aagxz14jzrxebcgr54xmktv1nf.bx.internal.cloudapp.net>
2022-10-20 13:56:14 +0000

    change transactionInput create generate random transaction function, rename mock_transactions/transactions models.py

---
+++ app/mock_transactions/models.py
@@ -0,0 +1,13 @@
+from columns import Columns
+from pydantic import BaseModel
+from enum import Enum
+
+class TransactionInput(BaseModel):
+    amount: int
+    hour: int
+    tag: str
+
+class TransactionType(Enum):
+    client = Columns.CLIENT 
+    supplier = Columns.SUPPLIER 
+    tax = Columns.TAX 
